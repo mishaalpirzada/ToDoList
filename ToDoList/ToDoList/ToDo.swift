a8844137aa8f9c2e4788412f9c754aec219fcee6
mishaalpirzada <68654868+mishaalpirzada@users.noreply.github.com>
2020-07-28 10:46:39 -0400

    finished iteration 0

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by mishaal pirzada on 7/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
